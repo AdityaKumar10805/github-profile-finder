38d1f5fb97450e676c08a136bbc50545329706d1
AdityaKumar10805 <adityak.ug24.cs@nitp.ac.in>
2026-06-08 14:54:10 +0530

    Add GitHub profile search and repositories feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("");
+  const [errors,setErrors]=useState("")
+  const[profile,setProfile]=useState(null)
+  const [repos, setRepos] = useState([]);
+  async function handleSearch(){
+    setErrors("")
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  if(!response.ok){
+    setProfile(null)
+    setErrors("user not found❌")
+    return
+  }
+  const re=await fetch(`https://api.github.com/users/${username}/repos`)
+  const data = await response.json();
+  const redata=await re.json()
+  console.log(redata)
+  const sortedRepos = [...redata].sort((a, b) =>
+    b.stargazers_count - a.stargazers_count
+  );
+  
+
+  setRepos(sortedRepos)
+  
+ 
+  
+  setProfile(data)
+
+
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
+    <div className="app">
+      <h1>GitHub Profile Finder 🔍</h1>
+
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Enter GitHub username..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <button onClick={handleSearch}disabled={!username}>
+          Search
         </button>
-      </section>
+      </div>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+      {errors &&<p className="error">{errors}</p>}
+      {profile &&(<><div className="card">
+        <img
+        
+          src={profile.avatar_url}
+          alt="avatar"
+        />
+
+        <h2>User Name</h2>
+        <p>@{username}</p>
+        <a href={profile.html_url} target="_blank">go to profile🌎</a>
+        <div className="stats">
+          <div>
+            <strong>Followers</strong>
+            <p>{profile.followers}</p>
+          </div>
+
+          <div>
+            <strong>Following</strong>
+            <p>{profile.following}</p>
+          </div>
+
+          <div>
+            <strong>Repos</strong>
+            <p>{profile.public_repos}</p>
+          </div>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+      </div>
+      <div className="repos">
+       <h3>Latest Repositories</h3>
+       
+        {repos.slice(0, 5).map((repo) => (
+        <div key={repo.id} className="repo-card">
+          <a href={repo.html_url} target="_blank">
+        {repo.name}
+        </a>
+         <p>{repo.description}</p>
         </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  ))}
+</div> </>)}
+    </div>
+  );
 }
 
-export default App
+export default App;
